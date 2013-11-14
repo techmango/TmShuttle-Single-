73eecab9a321588b5fec1968d71af8b75a6a5045
happy <jimmysu_88@163.com>
2013-11-14 08:32:52 -0800

    ddadd RegCodeGenerator

---
+++ RegCodeGenerator/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace RegCodeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_MvNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_SerialNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_MvNum
+            // 
+            this.txt_MvNum.Location = new System.Drawing.Point(106, 33);
+            this.txt_MvNum.Name = "txt_MvNum";
+            this.txt_MvNum.Size = new System.Drawing.Size(320, 21);
+            this.txt_MvNum.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "机器码：";
+            // 
+            // txt_SerialNumber
+            // 
+            this.txt_SerialNumber.Location = new System.Drawing.Point(106, 72);
+            this.txt_SerialNumber.Name = "txt_SerialNumber";
+            this.txt_SerialNumber.Size = new System.Drawing.Size(320, 21);
+            this.txt_SerialNumber.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "注册码：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "生成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 176);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_SerialNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_MvNum);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "注册码生成器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_MvNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_SerialNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RegCodeGenerator/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace RegCodeGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //txt_MvNum.Text = TmShuttle.RegisterUtility.getMvNum();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(!string.IsNullOrEmpty(txt_MvNum.Text.Trim()))
+                {
+                    txt_SerialNumber.Text = TmShuttle.RegisterUtility.getSerialNum(txt_MvNum.Text.Trim());
+                }
+                else
+                {
+                    MessageBox.Show("请输入机器码");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
